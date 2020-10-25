79a81c1cd3b29be0bdcf5c21a95b1a14185ce5ce
Alexandr b-nimaev <alexandrbnimaev@yandex.ru>
2020-10-25 17:07:17 +0800

    update

--- wp-content/themes/ar/functions.php
+++ wp-content/themes/ar/functions.php
@@ -525,3 +525,10 @@ function custom_titles( $title, $sep ) {
 
     return $title;
 }
+
+ function remove_css_id_filter ($var) {
+     return is_array ($var)?  array_intersect ($var, array ('current-menu-item')): '';
+ } 
+ add_filter ('page_css_class', 'remove_css_id_filter', 100, 1);
+ add_filter ('nav_menu_item_id', 'remove_css_id_filter', 100, 1);
+ add_filter ('nav_menu_css_class', 'remove_css_id_filter', 100, 1);
